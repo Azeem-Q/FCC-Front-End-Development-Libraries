de6bff9071b067630cd90f3f846fe990bdec60eb
Azeem <livelyazeem@gmail.com>
2022-04-20 17:21:11 +0500

    learning Target a Specific Child of an Element Using jQuery

--- jQueryScript.js
+++ jQueryScript.js
@@ -111,4 +111,39 @@ $(document).ready(function() {
 */
 
 // Target the Parent of an Element Using jQuery
+/*
+$(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+});
+*/
 
+// Target the Children of an Element Using jQuery
+/*
+$(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+    $("#right-well").children().css("color", "orange");
+});
+*/
+
+// Target a Specific Child of an Element Using jQuery
+
+$(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+    $("#right-well").children().css("color", "orange");
+    $(".well:nth-child(2)").addClass("animated bounce");
+});
