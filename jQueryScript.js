b2f2cab93bbf35f4a000d5589c731c0559f2518b
Azeem <livelyazeem@gmail.com>
2022-04-21 11:04:14 +0500

    learning Target Even Elements Using jQuery

--- jQueryScript.js
+++ jQueryScript.js
@@ -136,6 +136,20 @@ $(document).ready(function() {
 */
 
 // Target a Specific Child of an Element Using jQuery
+/*
+$(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+    $("#right-well").children().css("color", "orange");
+    $(".target:nth-child(2)").addClass("animated bounce");
+});
+*/
+
+// Target Even Elements Using jQuery
 
 $(document).ready(function() {
     $("#target1").css("color", "red");
@@ -145,5 +159,5 @@ $(document).ready(function() {
     $("#target5").clone().appendTo("#left-well");
     $("#target1").parent().css("background-color", "red");
     $("#right-well").children().css("color", "orange");
-    $(".well:nth-child(2)").addClass("animated bounce");
+    $(".target:nth-child(2)").addClass("animated bounce");
 });
