bc83fd7dd55c8f36b0b0eff0029d4656e1aa1c8f
Azeem <livelyazeem@gmail.com>
2022-04-21 11:38:33 +0500

    completed jQuery

--- jQueryScript.js
+++ jQueryScript.js
@@ -165,4 +165,18 @@ $(document).ready(function() {
 */
 
 // Use jQuery to Modify the Entire Page
+/*
+$(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+    $("#right-well").children().css("color", "orange");
+    $(".target:nth-child(2)").addClass("animated bounce");
+    $(".target:even").addClass("animated shakeX")
+    $("body").addClass("animated hinge");
+});
+*/
 
