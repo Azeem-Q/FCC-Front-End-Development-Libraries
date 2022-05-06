137aaa388ff5cb7797bdc418bb5ebcc6bb8e3a4d
Azeem <livelyazeem@gmail.com>
2022-05-06 07:08:48 +0500

    setting up react

---
+++ reactScript.jsx
@@ -0,0 +1,7 @@
+// Create a Simple JSX Element
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+const element = <h1>Hello JSX!</h1>;
+
+ReactDom.render(element, document.getElementById('root'));
