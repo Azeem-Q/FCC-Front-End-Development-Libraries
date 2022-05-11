25a989fc17e76bf6385dc8e256eb4db782a9a909
Azeem <livelyazeem@gmail.com>
2022-05-11 12:04:29 +0500

    finally set up production ready react -- feeling awesome

--- reactScript.js
+++ reactScript.js
@@ -1,12 +1,1 @@
 // Create a Simple JSX Element
-
-import React from 'react';
-import ReactDOM from 'react';
-
-const root = ReactDom.createRoot(document.getElementById('root'));
-
-
-
-const element = <h1>Hello JSX!</h1>;
-
-ReactDom.render(element, document.getElementById('root'));
