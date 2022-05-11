7c37a6cb93a5e7533613c0372d856d117325d998
Azeem <livelyazeem@gmail.com>
2022-05-11 13:02:22 +0500

    learning Create a Simple JSX Element

--- reactScript.js
+++ reactScript.js
@@ -1,1 +1,9 @@
 // Create a Simple JSX Element
+
+var JSX = React.createElement(
+  'h1',
+  null,
+  'Hello JSX!'
+);
+
+ReactDom.render(JSX, document.getElementById('root'));
--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -1,2 +1,5 @@
 // Create a Simple JSX Element
 
+const JSX = <h1>Hello JSX!</h1>;
+
+ReactDom.render(JSX, document.getElementById('root'));
