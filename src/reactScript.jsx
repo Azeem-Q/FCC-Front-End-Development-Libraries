a42297e65c20a67e7a4af2d034bf8d11e1446a27
Azeem <livelyazeem@gmail.com>
2022-05-11 19:22:14 +0500

    learned till Add Comments in JSX

--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -6,4 +6,20 @@ ReactDOM.render(JSX, document.getElementById('root'));
 */
 
 // Create a Complex JSX Element
+/*
+const JSX = 
+    <div>
+        <h1>React</h1>
+        <p>Create a complex JSX element</p>
+        <ul>
+            <li>Coding is fun</li>
+            <li>Its so awesome to finally come over a problem after a long struggle</li>
+            <li>Just some list item</li>
+        </ul>
+    </div>;
+
+ReactDOM.render(JSX, document.getElementById('root'));
+*/
+
+// Add Comments in JSX
 
