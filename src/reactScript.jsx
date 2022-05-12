aeb17ab1c382347dcf09e309e3fbefbea2ab24bb
Azeem <livelyazeem@gmail.com>
2022-05-12 12:38:18 +0500

    learned till Create a Stateless Functional Component

--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -58,4 +58,17 @@ ReactDOM.render(JSX, document.getElementById('challenge-node'));
 */}
 
 {/* Learn About Self-Closing JSX Tags */}
+{/*
+const JSX = (
+    <div>
+      <h2>Welcome to React!</h2> <br />
+      <p>Be sure to close all tags!</p>
+      <hr />
+    </div>
+  );
+
+ReactDOM.render(JSX, document.getElementById('challenge-node'));
+*/}
+
+{/*Create a Stateless Functional Component*/}
 
