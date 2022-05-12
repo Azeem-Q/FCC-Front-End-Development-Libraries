39e2e0b109486ef087c52bfa2d9d72ad73d92c75
Azeem <livelyazeem@gmail.com>
2022-05-12 11:22:32 +0500

    Learn About Self-Closing JSX Tags

--- reactScript.js
+++ reactScript.js
@@ -46,3 +46,14 @@ ReactDOM.render(JSX, document.getElementById('root'));
     */}
 
 {/* Define an HTML Class in JSX */}
+{/*
+    const JSX = (
+       <div className="myDiv">
+           <h1>Add a class to this div</h1>
+       </div>
+    );
+    
+    ReactDOM.render(JSX, document.getElementById('challenge-node'));
+    */}
+
+{/* Learn About Self-Closing JSX Tags */}
