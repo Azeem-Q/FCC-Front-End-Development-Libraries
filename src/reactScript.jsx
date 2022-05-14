e24aaec99b12b517e21a9cd2ac2c7ad1bf3b8a3d
Azeem <livelyazeem@gmail.com>
2022-05-14 08:48:57 +0500

    learned till Create a Component with Composition

--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -84,4 +84,22 @@ ReactDOM.render(<MyComponent />, document.getElementById('root'));
 */}
 
 {/*Create a React Component*/}
+{/*
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
+*/}
+
+{/*Create a Component with Composition*/}
 
