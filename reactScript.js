986018191112422c15875afbc79b77f24964e364
Azeem <livelyazeem@gmail.com>
2022-05-18 18:25:37 +0500

    learning Pass an Array as Props

--- reactScript.js
+++ reactScript.js
@@ -1,3 +1,11 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 // Create a Simple JSX Element
 /*
 const JSX = <h1>Hello JSX!</h1>;
@@ -365,3 +373,50 @@ ReactDOM.render(JSX, document.getElementById('root'));
     */}
 
 {/*Pass an Array as Props*/}
+
+var List = function List(props) {
+    return React.createElement('p', null);
+};
+
+var ToDo = function (_React$Component) {
+    _inherits(ToDo, _React$Component);
+
+    function ToDo(props) {
+        _classCallCheck(this, ToDo);
+
+        return _possibleConstructorReturn(this, (ToDo.__proto__ || Object.getPrototypeOf(ToDo)).call(this, props));
+    }
+
+    _createClass(ToDo, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h1',
+                    null,
+                    'To Do Lists'
+                ),
+                React.createElement(
+                    'h2',
+                    null,
+                    'Today'
+                ),
+                React.createElement(List, null),
+                React.createElement(
+                    'h2',
+                    null,
+                    'Tomorrow'
+                ),
+                React.createElement(List, null)
+            );
+        }
+    }]);
+
+    return ToDo;
+}(React.Component);
+
+;
+
+ReactDOM.render(React.createElement(ToDo, null), document.getElementById('challenge-node'));
--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -367,3 +367,25 @@ ReactDOM.render(<Calender />, document.getElementById('challenge-node'));
 
 {/*Pass an Array as Props*/}
 
+const List = (props) => {
+    return <p>{}</p>
+};
+
+class ToDo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>To Do Lists</h1>
+                <h2>Today</h2>
+                <List />
+                <h2>Tomorrow</h2>
+                <List />
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(<ToDo />, document.getElementById('challenge-node'));
