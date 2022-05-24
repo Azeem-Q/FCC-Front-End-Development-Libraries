8892f65226052149cf1f45523b447e8ae1a6f8b9
Azeem <livelyazeem@gmail.com>
2022-05-24 12:32:19 +0500

    learned till Bind 'this' to a Class Method

--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -589,4 +589,32 @@ ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
 */}
 
 {/*Set State with this.setState*/}
+{/*
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'Initial State'
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.setState({
+            name: 'React Rocks!'
+        })
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>Click Me</button>
+                <h1>{this.state.name}</h1>
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
+*/}
+
+{/*Bind 'this' to a Class Method*/}
 
