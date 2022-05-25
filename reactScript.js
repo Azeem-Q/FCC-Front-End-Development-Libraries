b5ae21f731c2ad9307cb8573bde0ec74dd0816a6
Azeem <livelyazeem@gmail.com>
2022-05-25 16:38:39 +0500

    still learning Bind 'this' to a Class Method

--- reactScript.js
+++ reactScript.js
@@ -1,3 +1,11 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 // Create a Simple JSX Element
 /*
 const JSX = <h1>Hello JSX!</h1>;
@@ -616,3 +624,49 @@ ReactDOM.render(JSX, document.getElementById('root'));
     */}
 
 {/*Bind 'this' to a Class Method*/}
+
+var MyComponent = function (_React$Component) {
+    _inherits(MyComponent, _React$Component);
+
+    function MyComponent(props) {
+        _classCallCheck(this, MyComponent);
+
+        var _this = _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this, props));
+
+        _this.state = {
+            text: 'Hello'
+        };
+        return _this;
+    }
+
+    _createClass(MyComponent, [{
+        key: 'handleClick',
+        value: function handleClick() {
+            this.setState({
+                text: 'You clicked!'
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'button',
+                    null,
+                    'Click Me'
+                ),
+                React.createElement(
+                    'h1',
+                    null,
+                    this.state.text
+                )
+            );
+        }
+    }]);
+
+    return MyComponent;
+}(React.Component);
+
+;
--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -618,3 +618,24 @@ ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
 
 {/*Bind 'this' to a Class Method*/}
 
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: 'Hello'
+        };
+    }
+    handleClick() {
+        this.setState({
+            text: 'You clicked!'
+        });
+    }
+    render() {
+        return (
+            <div>
+                <button>Click Me</button>
+                <h1>{this.state.text}</h1>
+            </div>
+        );
+    }
+};
