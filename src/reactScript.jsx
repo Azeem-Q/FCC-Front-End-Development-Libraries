e89ddc1dac2c407904e47e0f48add10d836e914d
Azeem <livelyazeem@gmail.com>
2022-06-01 15:25:38 +0500

    learned till Optimize Re-Renders with shouldComponentUpdate

--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -900,4 +900,73 @@ ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
 */}
 
 {/*Use the Lifecycle Method componentDidMount*/}
+{/*
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeUsers: null
+        };
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                activeUsers: 1273
+            });
+        }, 2500);
+    }
+    render() {
+        return (
+            <div>
+                <h1>Active Users: {this.state.activeUsers}</h1>
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
+*/}
+
+{/*Add Event Listeners*/}
+{/*
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ''
+        };
+        this.handleEnter = this.handleEnter.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyPress);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyPress);
+    }
+
+    handleEnter() {
+        this.setState((state) => ({
+            message: state.message + 'You pressed the enter key! '
+        }));
+    }
+    handleKeyPress(event) {
+        if (event.keyCode === 13) {
+            this.handleEnter();
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
+*/}
+
+{/*Optimize Re-Renders with shouldComponentUpdate*/}
 
