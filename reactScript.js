3fb93eda1b10484d7d4cfcd619424b7c14a39d87
Azeem <livelyazeem@gmail.com>
2022-06-02 16:36:15 +0500

    learned till Use a Ternary Expression for Conditional Rendering

--- reactScript.js
+++ reactScript.js
@@ -1129,3 +1129,67 @@ ReactDOM.render(JSX, document.getElementById('root'));
     */}
 
 {/*Render with an If-Else Condition*/}
+{/*
+    class MyComponent extends React.Component {
+       constructor(props) {
+           super(props);
+           this.state = {
+               display: true
+           }
+           this.toggleDisplay = this.toggleDisplay.bind(this);
+       }
+       toggleDisplay() {
+           this.setState((state) => ({
+               display: !state.display
+           }));
+       }
+       render() {
+           if (this.state.display == true) {
+               return (
+                   <div>
+                       <button onClick={this.toggleDisplay}>Toggle Display</button>
+                       <h1>Displayed!</h1>
+                   </div>
+                   );
+           } else {
+               return (
+                   <div>
+                       <button onClick={this.toggleDisplay}>Toggle Display</button>
+                   </div>
+               )
+           }
+       }
+    };
+    
+    ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
+    */}
+
+{/*Use && for a More Concise Conditional*/}
+{/*
+    class MyComponent extends React.Component {
+       constructor(props) {
+           super(props);
+           this.state = {
+               display: true
+           }
+           this.toggleDisplay = this.toggleDisplay.bind(this);
+       }
+       toggleDisplay() {
+           this.setState(state => ({
+               display: !state.display
+           }));
+       }
+       render() {
+           return (
+               <div>
+                   <button onClick={this.toggleDisplay}>Toggle Display</button>
+                   {this.state.display == true && <h1>Displayed!</h1>}
+               </div>
+           );
+       }
+    };
+    
+    ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
+    */}
+
+{/*Use a Ternary Expression for Conditional Rendering*/}
