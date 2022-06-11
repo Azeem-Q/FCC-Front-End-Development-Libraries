4b36957395642475d5d0e1650713d2fba011620c
Azeem <livelyazeem@gmail.com>
2022-06-11 12:47:11 +0500

    learned till Send Action Data to the Store

--- reactScript.js
+++ reactScript.js
@@ -1,3 +1,6 @@
+{} /*
+   const { string } = require("prop-types");
+   */
 // Create a Simple JSX Element
 {} /*
    const { bindActionCreators } = require("redux")
@@ -1647,3 +1650,51 @@ ReactDOM.render(JSX, document.getElementById('root'));
     */}
 
 {/*Combine Multiple Reducers*/}
+{/*
+    const INCREMENT = 'INCREMENT';
+    const DECREMENT = 'DECREMENT';
+    
+    const counterReducer = (state = 0, action) => {
+       switch(action.type) {
+           case INCREMENT:
+               return state + 1;
+           case DECREMENT:
+               return state - 1;
+           default:
+               return state;
+       }
+    };
+    
+    const LOGIN = 'LOGIN';
+    const LOGOUT = 'LOGOUT';
+    
+    const authReducer = (state = {authenticated: false}, action) => {
+       switch(action.type) {
+           case LOGIN:
+               return {
+                   authenticated: true
+               }
+           case LOGOUT:
+               return {
+                   authenticated: false
+               }
+           default:
+               return state;
+       }
+    };
+    
+    const rootReducer = Redux.combineReducers({
+       auth: authReducer,
+       count: counterReducer
+    });
+    
+    const store = Redux.createStore(rootReducer);
+    
+    console.log(store.getState());
+    store.dispatch({type: INCREMENT});
+    console.log(store.getState());
+    store.dispatch({type: LOGIN});
+    console.log(store.getState());
+    */}
+
+{/*Send Action Data to the Store*/}
