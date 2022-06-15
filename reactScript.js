bc277b3c33aee6e429faf298f0bde506d03eef86
Azeem <livelyazeem@gmail.com>
2022-06-15 17:33:22 +0500

    learning Remove an Item from an Array

--- reactScript.js
+++ reactScript.js
@@ -1,3 +1,5 @@
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 {} /*
    const { string } = require("prop-types");
    */
@@ -1876,3 +1878,28 @@ ReactDOM.render(JSX, document.getElementById('root'));
     */}
 
 {/*Remove an Item from an Array*/}
+
+var immutableReducer = function immutableReducer() {
+    var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [0, 1, 2, 3, 4, 5];
+    var action = arguments[1];
+
+    switch (action.type) {
+        case 'REMOVE_ITEM':
+            return [].concat(_toConsumableArray(state)).splice(action.index, 1);
+        default:
+            return state;
+    }
+};
+
+var removeItem = function removeItem(index) {
+    return {
+        type: 'REMOVE_ITEM',
+        index: index
+    };
+};
+
+var store = Redux.createStore(immutableReducer);
+
+console.log(store.getState());
+store.dispatch(removeItem(2));
+console.log(store.getState());
--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -1878,3 +1878,24 @@ const store = Redux.createStore(immutableReducer);
 
 {/*Remove an Item from an Array*/}
 
+const immutableReducer = (state = [0, 1, 2, 3, 4, 5], action) => {
+    switch(action.type) {
+        case 'REMOVE_ITEM':
+            return [...state].splice(action.index, 1);
+        default:
+            return state;
+    }
+};
+
+const removeItem = (index) => {
+    return {
+        type: 'REMOVE_ITEM',
+        index
+    };
+};
+
+const store = Redux.createStore(immutableReducer);
+
+console.log(store.getState());
+store.dispatch(removeItem(2));
+console.log(store.getState());
